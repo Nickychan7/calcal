387fa629e82104b9bd13d5f9ba8193b34c5a4a0e
Nickychan7 <nickychan.008@gmail.com>
2022-04-23 10:29:14 +0700

    23/04/22
    fix bug add body fat menu

--- js/main.js
+++ js/main.js
@@ -1,34 +1,64 @@
 const main = document.querySelector('.main');
-const calories = document.querySelector('.calories');
-const foodplan = document.querySelector('.foodplan');
-const bodyfat = document.querySelector('.bodyfat');
-
+const menu = document.querySelector('.menu');
+const calories = document.getElementById('calories');
+const foodplan = document.getElementById('foodplan');
+const bodyfat = document.getElementById('bodyfat');
+const div_form = document.querySelector('.div_form');
+const calories_form = document.querySelector('.calories_form');
+const bodyfat_form_simple = document.querySelector('.bodyfat_form_simple');
+const bodyfat_form_specified = document.querySelector('.bodyfat_form_specified');
+const simple_selector = document.getElementById('simple_bf_selector');
+const specified_selector = document.getElementById('specified_bf_selector');
 
 
 function init() {
+	menu.style.display = 'none';-
 	calories.style.display = 'none';
 	bodyfat.style.display = 'none';
 	foodplan.style.display = 'none';
-	radio_button();
+	div_form.style.display = 'none';
+	calories_form.style.display = 'none';
+	bodyfat_form_simple.style.display = 'none';
+	bodyfat_form_specified.style.display = 'none';
+}
+
+function refresh_bf(){
+	specified_selector.style.backgroundColor = 'transparent';
+	specified_selector.style.color = '#145369';
+	simple_selector.style.backgroundColor = 'transparent';
+	simple_selector.style.color = '#145369';
 }
 
 function show_option(option){
 	if (option == 1) {
+		menu.style.display = 'block';
 		calories.style.display = 'block';
+		div_form.style.display = 'block';
+		calories_form.style.display = 'block';
 		bodyfat.style.display = 'none';
 		foodplan.style.display = 'none';
+		bodyfat_form_simple.style.display = 'none';
+		bodyfat_form_specified.style.display = 'none';
+		radio_button();
+		refresh_bf();
 		calories.scrollIntoView();
 	}
 	else if(option == 2){
+		menu.style.display = 'block';
 		calories.style.display = 'none';
+		div_form.style.display = 'none';
 		bodyfat.style.display = 'block';
+		calories_form.style.display = 'none';
 		foodplan.style.display = 'none';
 		bodyfat.scrollIntoView();
 	}
 	else if(option == 3){
+		menu.style.display = 'block';
+		div_form.style.display = 'none';
 		calories.style.display = 'none';
 		bodyfat.style.display = 'none';
 		foodplan.style.display = 'block';
+		refresh_bf();
 		foodplan.scrollIntoView();
 	}
 	
@@ -60,7 +90,7 @@ function radio_button(){
 	var result = document.getElementById('result_calories');
 	var selector_calories = document.getElementById('selector_calories');
 	var calories_btn = document.getElementById('calories_btn');
-	var calories_title = document.getElementById('calories_title');
+	var calories_title = document.getElementById('calories');
 
 	for(i = 0; i < gender.length; i++) {
         if(gender[i].checked){
@@ -160,11 +190,15 @@ function count_calories(){
 	        else if (gender[i].value=="female"){
 	        	bmr = (447.6 + 9.25 * weight) + (3.1 * height) - (4.33 * age);
 	        }
-	        bmi = weight / (Math.pow((height/100),2));
+	        bmi = bmi_count(weight,height);
 	        show_result(bmr,get_exercise,bmi);
         }
         
     }
+}
+
+function bmi_count(weight,height){
+	return weight / (Math.pow((height/100),2));
 }
 
 function show_result(bmr,get_exercise,bmi){
@@ -233,5 +267,75 @@ function comma_number(num) {
 	return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, '.');
 }
 
+function bf_type(num){
+	div_form.style.display = 'block';
+	check_bf_type();
+	if (num == 0) {
+		bodyfat_form_simple.style.display = 'block';
+		bodyfat_form_specified.style.display = 'none';
+		simple_selector.style.backgroundColor = '#145369';
+		simple_selector.style.color = '#fff';
+		specified_selector.style.backgroundColor = 'transparent';
+		specified_selector.style.color = '#145369';
+		bodyfat_form_simple.scrollIntoView();
+	}
+	else if (num == 1) {
+		bodyfat_form_simple.style.display = 'none';
+		bodyfat_form_specified.style.display = 'block';
+		specified_selector.style.backgroundColor = '#145369';
+		specified_selector.style.color = '#fff';
+		simple_selector.style.backgroundColor = 'transparent';
+		simple_selector.style.color = '#145369';
+		bodyfat_form_simple.scrollIntoView();
+	}
+}
+
+function radio_bf_simple(){
+	var gender_simple = document.getElementsByName('gender_simple');
+	var female_label =document.getElementById('female_label_simple');
+	var male_label =document.getElementById('male_label_simple');
+	var result_bf_simple = document.getElementById('result_bf_simple');
+	var selector_bf_simple = document.getElementById('selector_bf_simple');
+	var bf_simple_btn = document.getElementById('bf_simple_btn');
+	var bodyfat_title = document.getElementById('bodyfat_title');
+	var simple_bf_type = document.getElementById('simple_bf_selector');
+	var specified_bf_type = document.getElementById('specified_bf_selector');
+
+	for(i = 0; i < gender_simple.length; i++) {
+		if (gender_simple[i].checked) {
+			if (gender_simple[i].value=="male") {
+				bf_simple_btn.style.backgroundColor = "#145369";
+        		selector_bf_simple.style.color = "#145369";
+        		bodyfat_title.style.color = "#145369";
+        		female_label.style.color = "#145369";
+        		male_label.style.color = "#145369";
+        		result_bf_simple.style.color = "#145369";	
+        		simple_bf_type.style.backgroundColor = "#145369";
+        		specified_bf_type.style.color = "#145369";		
+        	}
+			else if (gender_simple[i].value=="female") {
+				bf_simple_btn.style.backgroundColor = "#fc9483";
+	        	selector_bf_simple.style.color = "#fc9483";
+	        	bodyfat_title.style.color = "#fc9483";
+	        	female_label.style.color = "#fc9483";
+        		male_label.style.color = "#fc9483";
+	        	result_bf_simple.style.color = "#fc9483";
+	        	simple_bf_type.style.backgroundColor = "#fc9483";
+	        	specified_bf_type.style.color = "#fc9483";
+			}
+		}
+	}
+}
+
+function check_bf_type(){
+	document.getElementById('male_simple').checked = true;
+	document.getElementById('male_specified').checked = true;
+	radio_bf_simple();
+}
+
+function count_bf_simple(){
+	var result_bf_simple = document.getElementById('result_bf_simple');
+	result_bf_simple.innerHTML = "hasilnya blom ada";
+}
 
 init();
